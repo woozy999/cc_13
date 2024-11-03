287c32637a9767b37b33e178eb31b3587b29179e
k <kyle.offenbacher@gmail.com>
2024-11-03 17:24:35 -0500

    Fetch product data and display basic information.

---
+++ main.js
@@ -0,0 +1,10 @@
+fetch('https://www.course-api.com/javascript-store-products')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        displayProducts(data);
+    })
